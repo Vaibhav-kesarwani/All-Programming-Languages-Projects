a7ae281088531b2d0a09c00bb124a6c52f99f097
Vaibhav Kesarwani <116189379+Vaibhav-kesarwani@users.noreply.github.com>
2022-11-14 21:30:53 +0530

    Add files via upload

---
+++ C++/Day_2.cpp
@@ -0,0 +1,13 @@
+// This code is written by Vaibhav Kesarwani
+#include <iostream>
+
+using namespace std;
+int main()
+{
+    int a = 6;
+    int b = 5;
+    int sum = a + b;
+    cout << "The sum of a+b is-->" << sum << endl;
+
+    return 0;
+}
---
+++ C++/Day_3.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+int main()
+{
+    int a = 5;
+    float b = 6.2;
+    char c = 'V';
+    double d = 3.14;
+    bool bl = true;
+
+    cout << a << endl;
+    cout << b << endl;
+    cout << c << endl;
+    cout << d << endl;
+    cout << bl << endl;
+
+    return 0;
+}
